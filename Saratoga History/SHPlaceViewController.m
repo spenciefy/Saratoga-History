77f910bb660b66411eb9831553bf9623c4b756fa
Spencer Yen <spencersyen@gmail.com>
2015-01-02 22:20:29 -0800

    fine added placevc.m

--- Saratoga History/SHPlaceViewController.m
+++ Saratoga History/SHPlaceViewController.m
@@ -16,7 +16,7 @@
 @implementation SHPlaceViewController {
     UIScrollView *scrollView;
     UILabel *titleLabel;
-
+    
 }
 
 - (void)viewDidLoad {
@@ -29,28 +29,33 @@
     scrollView.scrollEnabled = YES;
     scrollView.userInteractionEnabled = YES;
     [self.view addSubview:scrollView];
-//    scrollView.contentSize = CGSizeMake(width,height);
-
+    //    scrollView.contentSize = CGSizeMake(width,height);
     
-    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, self.view.frame.size.width, 40)];
+    
+    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, self.view.frame.size.width - 15, 40)];
     titleLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:22.0f];
+    titleLabel.center = CGPointMake(self.view.frame.size.width/2, 25);
     titleLabel.textAlignment = NSTextAlignmentLeft;
     titleLabel.text = self.place.placeTitle;
     [scrollView addSubview:titleLabel];
     
-    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:[[NSBundle mainBundle] URLForResource:@"I Need A Dollar" withExtension:@"mp3"] options:nil];
-
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"I Need A Dollar" ofType:@"mp3"];
+    NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+    SYAudioPlayerView *audioPlayer = [[SYAudioPlayerView alloc] initWithFrame:CGRectMake(0,0,self.view.frame.size.width - 10, 45)
+                                                                 audioFileURL:fileURL
+                                                                     autoplay:NO];
+    audioPlayer.center = CGPointMake(self.view.frame.size.width/2, 48);
+    [scrollView addSubview:audioPlayer];
     
-    self.playerView = [[SYWaveformPlayerView alloc] initWithFrame:CGRectMake(3,41,self.view.frame.size.width - 10, 45) asset:asset color:[UIColor lightGrayColor] progressColor:[UIColor colorWithRed:1 green:0.2 blue:0.2 alpha:1]];
-    [scrollView addSubview:self.playerView];
+    self.audioVC.view.progressView.foregroundColor = [UIColor colorWithRed:88/255. green:199/255. blue:226/255. alpha:1];
+    self.audioVC.view.progressView.backgroundColor = [UIColor colorWithWhite:207/255. alpha:1];
+    self.audioVC.view.backgroundColor = [UIColor colorWithWhite:238/255. alpha:1];
+    self.audioVC.view.playbackTimeLabel.textColor = [UIColor colorWithWhite:102/255. alpha:1];
+    self.audioVC.view.titleLabel.textColor = [UIColor colorWithWhite:102/255. alpha:1];
+    [self.audioVC playAudioWithURL:self.place.audioURLAsset.URL];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(pause)
-                                                 name:@"PageViewChange"
-                                               object:nil];
-    
-    EScrollerView *imageScroller = [[EScrollerView alloc] initWithFrameRect:CGRectMake(0, 90, self.view.frame.size.width, 200)
-                                                          ImageArray:self.place.images
+    EScrollerView *imageScroller = [[EScrollerView alloc] initWithFrameRect:CGRectMake(0, 75, self.view.frame.size.width, 200)
+                                                                 ImageArray:self.place.images
                                                                  TitleArray:self.place.imageCaptions];
     imageScroller.delegate = self;
     [self.view addSubview:imageScroller];
@@ -67,7 +72,7 @@
 }
 
 - (void)pause {
-    [self.playerView pause];
+    [self.playerView stopAudio:self];
 }
 
 - (IBAction)expand:(id)sender {
